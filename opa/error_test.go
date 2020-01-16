ebc32bc1e535416d5a8d872d793bf3dfbe87f626
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-16 17:47:10 +0100

    chore(test): fix test

--- opa/error_test.go
+++ opa/error_test.go
@@ -17,17 +17,3 @@ func TestIsNotFoundErr(T *testing.T) {
 	err := errors.New("not a 404")
 	assert.False(IsNotFoundErr(err))
 }
-
-func TestIsNotFoundErrE2E(T *testing.T) {
-
-	assert := assert.New(T)
-
-	client, err := NewClient(nil, nil)
-	assert.Nil(err)
-	assert.NotNil(client)
-
-	consumer, err := client.Policies.Get(defaultCtx, String("does-not-exists"))
-	assert.Nil(consumer)
-	assert.NotNil(err)
-	assert.True(IsNotFoundErr(err))
-}
