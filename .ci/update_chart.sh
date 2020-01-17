dfe758106f5f3373c6c4804da3ac9060922ba090
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-17 17:12:49 +0100

    fix(chart): fix release version

--- .ci/update_chart.sh
+++ .ci/update_chart.sh
@@ -3,11 +3,11 @@
 NAME=unknown
 TEAM=unknown
 REPOSITORY=unknown
-SHA=$(cat uropa-$TRAVIS_TAG.tgz | git hash-object --stdin)
-CONTENT=$(cat uropa-$TRAVIS_TAG.tgz | base64)
+SHA=$(cat $NAME-$VERSION.tgz | git hash-object --stdin)
+CONTENT=$(cat $NAME-$VERSION.tgz | base64)
 
 curl -X PUT -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" \
-  "https://api.github.com/repos/$TEAM/$REPOSITORY/contents/$NAME-$TRAVIS_TAG.tgz" \
+  "https://api.github.com/repos/$TEAM/$REPOSITORY/contents/$NAME-$VERSION.tgz" \
   -d '{
         "message":"Chart update",
         "committer": {
