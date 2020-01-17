aa21189cdaa7c398813cdd3fb8a7c6fe920fdfc6
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-17 18:01:05 +0100

    fix(chart): update chart

--- .ci/update_chart.sh
+++ .ci/update_chart.sh
@@ -7,12 +7,12 @@ echo "Upload chart $NAME"
 
 curl -X PUT -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" \
   "https://api.github.com/repos/$TEAM/$REPOSITORY/contents/$NAME-$VERSION.tgz" \
-  -d '{
+  -d "{
         "message":"Chart update",
         "committer": {
           "name": "Chart Release Bot",
           "email": "charts@ninjaneers.de"
         },
-        "content":"'$CONTENT'",
-        "sha":"'$SHA'"
-      }'
+        "content":"$CONTENT",
+        "sha":"$SHA"
+      }"
