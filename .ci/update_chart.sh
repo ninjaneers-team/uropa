390f2e7092ff4867163affe2733fe7a7d4b93213
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-22 13:58:38 +0100

    chore(upload): upload huge charts

--- .ci/update_chart.sh
+++ .ci/update_chart.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -ex
 
-CONTENT=$(base64 $NAME-$VERSION.tgz | tr -d \\n)
-
 echo "Upload chart $NAME"
 
 curl -X PUT -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" \
   "https://api.github.com/repos/$TEAM/$REPOSITORY/contents/$NAME-$VERSION.tgz" \
-  -d "{
+  --data @<(printf '%s' "{
         \"message\":\"Chart update\",
         \"committer\": {
           \"name\": \"Chart Release Bot\",
           \"email\": \"charts@ninjaneers.de\"
         },
-        \"content\":\"$CONTENT\"
-      }"
+        \"content\":\"$(base64 $NAME-$VERSION.tgz | tr -d \\n)\"
+      }") > /dev/null
